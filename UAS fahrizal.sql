e61d4992aa3c46fbf8104b0621d35a4b0aeb6923
fahrizal78 <129971684+fahrizal78@users.noreply.github.com>
2023-07-04 10:03:06 +0700

    Add files via upload

---
+++ UAS fahrizal.sql
@@ -0,0 +1,13 @@
+use db_pratikum;
+
+-- soal 1
+select kode_pelanggan, nama_produk, qty, harga, qty*harga from tr_penjualan_dqlab where qty*harga >= 200000 order by qty*harga;
+
+-- soal 2
+select tp.kode_transaksi, mp.kode_pelanggan, tp.kode_produk, mp.nama_pelanggan, nama_produk, tp.harga, tp.qty, tp.qty*harga as total
+from tr_penjualan_dqlab as tp join ms_pelanggan_dqlab as mp 
+on tp.kode_pelanggan = mp.kode_pelanggan
+where qty >= 3 order by qty;
+
+-- soal 3
+select nama_produk, tgl_transaksi from tr_penjualan_dqlab where tgl_transaksi order by tgl_transaksi;
